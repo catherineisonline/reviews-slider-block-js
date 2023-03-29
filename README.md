b0b5dcf74a56071523dc7803f37e50bd9276b04b
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-03-29 17:36:34 -0300

    renewed some code

---
+++ README.md
@@ -0,0 +1,13 @@
+![Reviews Slider Block]()
+
+# Reviews Slider Block
+
+
+[Live Preview](https://reviews-slider-block-js.vercel.app/)
+
+## About The Project
+This is a simple and minimalistic reviews made to learn basics of JavaSript and how to interact with DOM.
+
+## Built with
+- Semantic HTML5 markup
+- Flex
--- app.js
+++ app.js
@@ -3,45 +3,45 @@
 const reviews = [
   {
     id: 1,
-    name: "Ana Kowalski",
+    name: "Ana Kowalski,",
     job: "Web Developer",
     img: "https://images.generated.photos/MfG1uL9_lwaXj_I7A4z4I6rC8DWbUH0MpWyxDu9eMVI/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTQ1NzEyLmpwZw.jpg",
-    text: "This is the best app I have used to manage customer reviews. It is very customizable with a ton of options. I love the customer service. They are lighting fast with any questions I have and provide great support when needed. I highly recommend it....",
+    text: "The pizza at this restaurant is absolutely delicious! The crust is perfectly crispy and the toppings are fresh and flavorful. I highly recommend trying the margherita pizza.    ",
   },
   {
     id: 2,
     name: "Lisa Denson",
     job: "Designer",
     img: "https://images.generated.photos/R7J_x_Eiz8hOa4PBdy4az0OXGtPgxBuFnSv77qUrv3Y/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODg5Njc3LmpwZw.jpg",
-    text: "Great app! All I need now is a fully translation into another language (german in my case). You can easily change it but the date for instance stays in english and also in the american format MM/DD/YYY",
+    text: "The service at this restaurant is top-notch. The staff is friendly and attentive, making sure that you have everything you need to enjoy your meal. Plus, the atmosphere is cozy and welcoming.",
   },
   {
     id: 3,
     name: "Sarah Smith",
     job: "Team Manager",
     img: "https://images.generated.photos/YUtdEs4bTngHiYaZPwpyOCjACif6oE3ticPNV5L2WjU/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NTMzNzU5LmpwZw.jpg",
-    text: "Same as most disappointed review, the app did not work, duplicating on my product page. And when I deleted the APP, the review title, the write a comment and previous comments are still on my page, although the app has been deleted. I can't seem...",
+    text: "This pizza restaurant has some of the best deals in town. Their lunch specials are a steal, and you get a lot of food for your money. I love that I can get a delicious pizza without breaking the bank.",
   },
   {
     id: 4,
-    name: "Nick Cole",
+    name: "Nina Cole",
     job: "Project Manager",
     img: "https://images.generated.photos/yJs3AV7c22oNfG-YhiF1BFk-aQEwVgGuVYpP7ZFiXy8/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTMzODg2LmpwZw.jpg",
-    text: "I love the app but I am still trying to preview fewer reviews per page.On my product pages, 5 reviews are shown and I would like to show only 2 or 3. Is it even possible to modify this? and How? Thanks!",
+    text: "I've been coming to this restaurant for years and it never disappoints. The consistency in the quality of their pizza is impressive. Whether I'm dining in or ordering takeout, I know I'll always get a great meal.",
   },
   {
     id: 5,
     name: "Kim Yong",
     job: "Marketing Specialist",
     img: "https://images.generated.photos/iPR0DwYRyOx_S8XuZKGYnyEDnEw97BChQDFCkLNsqhs/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/OTYxNTMwLmpwZw.jpg",
-    text: "This app is TOP NOTCH!! love it, simple and clean and the SUPPORT is 10000% on point! responsive and FIXES any issues I've had! HIGHLY RECOMMENDED and you can't beat free",
+    text: "If you're a fan of spicy food, you have to try their 'spicy pepperoni' pizza. It's got the perfect amount of heat and the flavor is amazing. It's definitely one of my favorite pizzas on the menu.",
   },
   {
     id: 6,
     name: "Cindy Kane",
     job: "Sales Manager",
     img: "https://images.generated.photos/CWGIBk0LxK1i7q7nCILxSMuL9VULSfVhkYL4gOHwt2Q/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTk2MjU2LmpwZw.jpg",
-    text: "A very useful app for providing some simple social proof for your products. It would be very if the reviews were translated by translation apps that are installed on your site as this doesn't appear to be possible.",
+    text: "I recently tried the gluten-free pizza at this restaurant and it was fantastic. It's hard to find good gluten-free options, but this pizza exceeded my expectations. The crust was crispy and the toppings were delicious. I'm so glad I found a place where I can still enjoy pizza without any gluten.",
   },
 ];
 
@@ -91,12 +91,3 @@ prevBtn.addEventListener("click", function () {
   }
   showPerson();
 });
-
-//random person
-
-/*
-randomBtn.addEventListener("click", function () {
-    currentItem = Math.floor(Math.random() * reviews.length)
-    showPerson();
-});
-*/
